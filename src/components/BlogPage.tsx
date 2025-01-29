a06f3d19bff5a95cadf1b435f885a87341712e17
Rawan Abdellatif <121334455+Rawan-Abdellatif@users.noreply.github.com>
2025-01-29 09:15:40 -0700

    Update BlogPage.tsx

--- src/components/BlogPage.tsx
+++ src/components/BlogPage.tsx
@@ -66,7 +66,7 @@ const BlogPage: React.FC = () => {
         <div className={styles.blogContainer}>
          
      {blogPosts.map((post) => (
-          <div className="card">
+          <div className="card__body">
         <div key={post.title} className={styles.container}>
       
        <div className={styles.title}>
